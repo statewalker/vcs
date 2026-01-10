aa47c4f7fc2f37ec7c8b7725e318bd1fa32f04bd
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2026-01-10 17:23:00 +0100

    bd daemon sync: 2026-01-10 17:23:00

--- packages/core/src/delta/index.ts
+++ packages/core/src/delta/index.ts
@@ -1,17 +1,3 @@
-// Core interfaces
-export * from "./candidate-finder.js";
-export * from "./delta-compressor.js";
-export * from "./delta-decision-strategy.js";
-export * from "./delta-engine.js";
-export * from "./delta-storage.js";
-
-// Implementations
-export * from "./candidate-finder/index.js";
-export * from "./compressor/index.js";
-export * from "./engine/index.js";
-export * from "./strategy/index.js";
-
-// Legacy exports (still used)
 export * from "./delta-binary-format.js";
 export * from "./delta-store.js";
 export * from "./gc-controller.js";
