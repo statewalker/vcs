389f44de80f3970125bb2e9b3760a9bacec11426
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2026-01-21 03:05:10 +0100

    bd daemon sync: 2026-01-21 00:05:10

--- packages/transport/src/socket/index.ts
+++
@@ -1,6 +0,0 @@
-// Socket transport layer - bidirectional socket, client and server
-export {
-  createBidirectionalSocket,
-  createBidirectionalSocketPair,
-} from "./bidirectional-socket.js";
-export type { BidirectionalSocket, BidirectionalSocketOptions } from "./types.js";
